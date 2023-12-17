e7158ae998566da10c39d92f095ccf93791c2e01
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-12-17 14:41:10 +0530

    add docker compose to init mongo database

--- src/server/config.ts
+++ src/server/config.ts
@@ -5,8 +5,13 @@ export const PORT = env.PORT ?? "8080";
 export const HOST = env.host ?? "0.0.0.0";
 export const SERVER_URL = `http://${HOST}:${PORT}`;
 
-export default{
+export const MONGO_URI = env.MONGO_URI ?? "mongodb://localhost:27017";
+export const DATABASE_NAME = env.DATABSE_NAME ?? "local";
+
+export default {
     PORT,
     HOST,
-    SERVER_URL
+    SERVER_URL,
+    MONGO_URI,
+    DATABASE_NAME
 }
