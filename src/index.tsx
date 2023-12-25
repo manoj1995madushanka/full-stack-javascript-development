1d6ba40fabbb4b94140972468b9b220f9c37eecc
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-12-25 19:40:02 +0530

    handle conditions

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,22 @@
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
+
+const TODOS = ["Learn React", "Build Something"]
 
 const App = () => {
-    return <div>Hello React</div>;
+    return (
+        <div>
+            <div style={{color: "green"}} className="container" title="Hello React">
+                <h1>Hello React</h1>
+            </div>
+            {Math.random() > 0.5 && <h2>With JSX</h2>}
+            {TODOS.map((element) => {
+                return <li>{element}</li>
+            })}
+        </div>
+    )
 };
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(<App/>);
